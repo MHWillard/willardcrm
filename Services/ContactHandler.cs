ae3338db6cd47452a01dca41811776ed16d1f04e
MHWillard <mhwillard@gmail.com>
2023-10-15 16:43:01 -0400

    .

--- Services/ContactHandler.cs
+++ Services/ContactHandler.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
+using System.IO;
 using willardcrm.DataModel;
 
 namespace willardcrm.Services
@@ -13,14 +15,16 @@ namespace willardcrm.Services
         public string GetContactJSON(string contactName)
         {
             //return "{\"name\": \"Bill Grogs\",\"relationship\": \"Friend\",\"interests\": \"roleplaying games, history\",\"email:\"bill@billgrognard.com\",\"number\":\"555.782.9843\",\"notes\":\"Bill's website is billgrognard.com. Interesting articles about programming, roleplaying games, and why he hates traffic.\",\"updates:\" []}";
-            string output = JsonConvert.SerializeObject(contactName);
-            System.Diagnostics.Debug.WriteLine(output);
+            //open JSON file
+            //string output = JsonConvert.SerializeObject(dummyJSON);
+            string output = File.ReadAllText("./TestingData/testBill.json");
+            Trace.WriteLine(output);
             return output;
         }
 
         public void saveContact(ContactItem contact)
         {
-            System.Diagnostics.Debug.WriteLine("contact saved");
+            Trace.WriteLine("contact saved");
             //take this particular contact info and write it to a JSON object or string object
             //get each property
             //convert into .json
