f10351451b8006efc24eda92fc7972ae075ac799
MHWillard <mhwillard@gmail.com>
2023-11-10 14:16:38 -0500

    daily commit

--- ViewModels/ContactListViewModel.cs
+++ ViewModels/ContactListViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using ReactiveUI;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using willardcrm.DataModel;
 
@@ -6,11 +7,24 @@ namespace willardcrm.ViewModels
 {
     public class ContactListViewModel : ViewModelBase
     {
+        ContactItem selectedItem;
+        ContactDetailsViewModel detailsViewModel;
+
         public ContactListViewModel(IEnumerable<ContactItem> items)
         {
             ListItems = new ObservableCollection<ContactItem>(items);
         }
 
         public ObservableCollection<ContactItem> ListItems { get; }
+
+        public ContactItem SelectedItem
+        {
+            get => selectedItem;
+            set 
+            { 
+                this.RaiseAndSetIfChanged(ref selectedItem, value);
+                detailsViewModel.ReceivedItem = value;
+            }
+        }
     }
 }
